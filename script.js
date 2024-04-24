de70438de8e0bd87ecd9b3c660347737ed6cd0fc
AlissCava <cavalleraalissa@gmail.com>
2024-04-24 12:56:06 +0200

    if you dont input anithing

--- script.js
+++ script.js
@@ -1,1 +1,10 @@
 'use strict';
+
+//the check class with the click action start the function
+document.querySelector('.check'),addEventListener('click', function() {
+    const guess = Number (document.querySelector('.guess').value); //imput numeber -> save in the guess const variable
+    
+    if (!guess){
+        displayMessage('😒 No number!');
+    }
+}) 
