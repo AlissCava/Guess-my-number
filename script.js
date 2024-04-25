52de25f893e5d4351d735dd3469a5eb86d659cef
AlissCava <cavalleraalissa@gmail.com>
2024-04-25 10:11:57 +0200

    generate random number and guess logic

--- script.js
+++ script.js
@@ -1,10 +1,22 @@
 'use strict';
 
+const secretNumber = Math.trunc(Math.random() * 20 ) + 1;
+document.querySelector('.number').textContent = secretNumber;
+
 //the check class with the click action start the function
 document.querySelector('.check'),addEventListener('click', function() {
     const guess = Number (document.querySelector('.guess').value); //imput numeber -> save in the guess const variable
     
     if (!guess){
-        displayMessage('😒 No number!');
+        document.querySelector('.message').textContent = '😒 No number!';
+    }
+    else if(guess === secretNumber){
+        document.querySelector('.message').textContent = '🎉🎉WOOOOOOO PERFECT';
+    }
+    else if (guess < secretNumber){
+        document.querySelector('.message').textContent = '📉noo it is to low';
+    }
+    else if (guess > secretNumber){
+        document.querySelector('.message').textContent = '📈noo it is to hight';
     }
 }) 
