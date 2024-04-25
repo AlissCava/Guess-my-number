762ba6d5c4017eaff893f336f4c75eb66be993d6
AlissCava <cavalleraalissa@gmail.com>
2024-04-25 10:44:30 +0200

    score controll

--- script.js
+++ script.js
@@ -1,6 +1,11 @@
 'use strict';
 
+
 const secretNumber = Math.trunc(Math.random() * 20 ) + 1;
+
+let score = 20;
+let highscore = 0;
+
 document.querySelector('.number').textContent = secretNumber;
 
 //the check class with the click action start the function
@@ -14,9 +19,26 @@ document.querySelector('.check'),addEventListener('click', function() {
         document.querySelector('.message').textContent = '🎉🎉WOOOOOOO PERFECT';
     }
     else if (guess < secretNumber){
-        document.querySelector('.message').textContent = '📉noo it is to low';
+        if(score > 1){
+            document.querySelector('.message').textContent = '📉noo it is to low';
+            score--;
+            document.querySelector('.score').textContent = score;
+        }  
+        else {
+            document.querySelector('.message').textContent = '😭😭oh noo, you lost';
+            document.querySelector('.score').textContent = 0;
+        } 
     }
     else if (guess > secretNumber){
-        document.querySelector('.message').textContent = '📈noo it is to hight';
+        if ( score > 1 ){
+            document.querySelector('.message').textContent = '📈noo it is to hight';
+            score--;
+            document.querySelector('.score').textContent = score;
+        }
+        else {
+            document.querySelector('.message').textContent = '😭😭oh noo, you lost';
+            document.querySelector('.score').textContent = 0;
+        } 
+       
     }
 }) 
